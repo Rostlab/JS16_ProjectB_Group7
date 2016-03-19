724833ab349ea760e6495f3a5adfecf576bca50d
Daniel <daniel.albert@tum.de>
2016-03-19 17:55:57 +0100

    added character name conversion in npm.js/getPlod()  - added npm.js to grunt tests  - fixed minor errors in npm.js issued by grunt

--- npm.js
+++ npm.js
@@ -1,17 +1,21 @@
 module.exports = (function() {
 	// remember to call init before using the object
 	// to work it requires the file prediction.json to be in the current folder
-	const fs = require('fs');
+	var fs = require('fs');
+
 	this.data = {};
+
 	this.init = function() {
 		json_char = fs.readFileSync('./prediction.json','utf8');
 		array = JSON.parse(json_char);
-		for (id in array) {
+		for (var id in array) {
 			char = array[id];
 			this.data[char.name] = char.plod;
 		}
 	};
-	this.getPlod = function(name) {
+
+	this.getPlod = function(character_name) {
+		var name = character_name.toString().toLowerCase().replace(/[\'()]/g, '');
 		return parseFloat(this.data[name]);
 	};
 
