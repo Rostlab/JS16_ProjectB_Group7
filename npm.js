71450fc78f2c05baa6e6f45576985c5f20569a0b
nicoladesocio <nikdeso@hotmail.it>
2016-03-19 14:21:44 +0100

    correction for require

--- npm.js
+++ npm.js
@@ -1,8 +1,7 @@
-fs = require('fs');
-
 module.exports = (function() {
 	// remember to call init before using the object
 	// to work it requires the file prediction.json to be in the current folder
+	const fs = require('fs');
 	this.data = {};
 	this.init = function() {
 		json_char = fs.readFileSync('./prediction.json','utf8');
